d2fb1d72d5a798062ae921cdbce6b20e8fdb378e
mirza@t4nix <dev@mrz.my.id>
2025-07-23 18:44:28 +0700

    refactor(prober): move proto file to root

--- prober/build.rs
+++ prober/build.rs
@@ -1,6 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["proto/message.proto"], &["proto/"])?;
+    prost_build::compile_protos(&["../protobuf/message.proto"], &["../protobuf/"])?;
     Ok(())
 }
