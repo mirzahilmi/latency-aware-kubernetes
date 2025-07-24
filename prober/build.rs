615750be8f1cd41443aa7922c67cbf10e7d75407
mirza@t4nix <dev@mrz.my.id>
2025-07-24 21:25:16 +0700

    feat(prober): enable parallel ping with support for multiple target

--- prober/build.rs
+++ prober/build.rs
@@ -1,6 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["../protobuf/message.proto"], &["../protobuf/"])?;
+    prost_build::compile_protos(&["protobuf/message.proto"], &["protobuf/"])?;
     Ok(())
 }
--- prober/src/lib.rs
+++ prober/src/lib.rs
@@ -1,56 +1,80 @@
 use regex::Regex;
-use std::{process::Command, vec};
+use std::{error::Error, process::Command, sync::Arc, thread, vec};
+
+use crate::prober::Latency;
 
 pub mod prober {
     include!(concat!(env!("OUT_DIR"), "/prober.rs"));
 }
 
-pub type Error = Box<dyn std::error::Error>;
-
 pub trait Prober {
-    fn ping(&self, n: usize) -> Result<Vec<f32>, Error>;
+    fn ping(&self, n: usize) -> Result<Vec<Latency>, Box<dyn Error>>;
 }
 
-// this one actually the usual pattern i do when writing golang, im not sure if this also
-// appropriate when writing rust, but at the time i write this, it is what it is
-pub fn new_prober(target: String) -> impl Prober {
-    Ping { target }
-}
-
-struct Ping {
-    target: String,
+pub struct Ping {
+    pub targets: Vec<String>,
 }
 
 impl Prober for Ping {
-    fn ping(&self, n: usize) -> Result<Vec<f32>, Error> {
-        let out = Command::new("ping")
-            .arg("-c")
-            .arg(format!("{n}"))
-            .arg(&self.target)
-            .output()
-            .unwrap();
-        let out = String::from_utf8(out.stdout).unwrap();
+    fn ping(&self, n: usize) -> Result<Vec<Latency>, Box<dyn Error>> {
+        let mut tasks = vec![];
+        let pattern = Arc::new(Regex::new(r"(?m)(?:time=){1}(.+)(?:\sms){1}$")?);
 
-        let entries: String = out
-            .split("\n")
-            .enumerate()
-            .filter(|(i, _)| (1..n + 1).contains(i))
-            .map(|(_, v)| -> String {
-                let mut v = String::from(v);
-                v.push('\n');
-                v
-            })
-            .collect::<Vec<String>>()
-            .concat();
+        for target in self.targets.clone() {
+            let pattern = Arc::clone(&pattern);
+            tasks.push(thread::spawn(move || -> (String, Vec<f32>) {
+                let out = Command::new("ping")
+                    .arg("-c")
+                    .arg(format!("{}", &n))
+                    .arg(&target)
+                    .output()
+                    .unwrap();
+                let out = String::from_utf8(out.stdout).unwrap();
 
-        let pattern = Regex::new(r"(?m)(?:time=){1}(.+)(?:\sms){1}$").unwrap();
+                let entries: String = out
+                    .split("\n")
+                    .enumerate()
+                    .filter(|(i, _)| (1..n + 1).contains(i))
+                    .map(|(_, v)| -> String {
+                        let mut v = String::from(v);
+                        v.push('\n');
+                        v
+                    })
+                    .collect::<Vec<String>>()
+                    .concat();
 
-        let mut metrics = vec![];
-        for (_, [metric]) in pattern.captures_iter(&entries).map(|c| c.extract()) {
-            let metric: f32 = metric.parse()?;
-            metrics.push(metric);
+                let mut metrics = vec![];
+                for (_, [metric]) in pattern.captures_iter(&entries).map(|c| c.extract()) {
+                    let metric: f32 = metric.parse().unwrap();
+                    metrics.push(metric);
+                }
+
+                (target, metrics)
+            }));
         }
 
-        Ok(metrics)
+        let mut latencies = vec![];
+        for task in tasks {
+            let (target, metrics) = match task.join() {
+                Ok(it) => it,
+                Err(err) => {
+                    let msg = match err.downcast_ref::<&'static str>() {
+                        Some(s) => *s,
+                        None => match err.downcast_ref::<String>() {
+                            Some(s) => &s[..],
+                            None => "Sorry, unknown payload type",
+                        },
+                    };
+                    println!("Thread panics: {msg}");
+                    continue;
+                }
+            };
+            latencies.push(Latency {
+                ip_destination: target,
+                metrics,
+            });
+        }
+
+        Ok(latencies)
     }
 }
