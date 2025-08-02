e5cdbb6bdaed96208542475f0353645261e09776
mirza@t4nix <dev@mrz.my.id>
2025-08-02 19:23:49 +0700

    refactor(hellopod): sort meta, fix spacing & set favicon to 😛

--- hellopod/src/main.rs
+++ hellopod/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env};
+use std::env;
 
 use dioxus::prelude::*;
 
@@ -19,39 +19,38 @@ fn App() -> Element {
     let node_name = env::var("NODE_NAME").unwrap_or("N/A".to_string());
     let node_ip = env::var("NODE_IP").unwrap_or("N/A".to_string());
 
-    let mut metas = HashMap::new();
-    metas.insert("Node Name".to_string(), node_name);
-    metas.insert("Node IP".to_string(), node_ip);
-    metas.insert("Pod Name".to_string(), pod_name);
-    metas.insert("Pod Namespace".to_string(), pod_ns);
-    metas.insert("Pod IP".to_string(), pod_ip);
-    metas.insert("Pod Memory Limit".to_string(), pod_mem_limit);
-    metas.insert("Pod CPU Limit".to_string(), pod_cpu_limit);
+    let metas = vec![
+        ("Node Name".to_string(), node_name),
+        ("Node IP".to_string(), node_ip),
+        ("Pod Name".to_string(), pod_name),
+        ("Pod Namespace".to_string(), pod_ns),
+        ("Pod IP".to_string(), pod_ip),
+        ("Pod Memory Limit".to_string(), pod_mem_limit),
+        ("Pod CPU Limit".to_string(), pod_cpu_limit),
+    ];
 
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Stylesheet { href: TAILWIND_CSS }
         div { class: "bg-gray-900 min-h-screen flex flex-col items-center justify-center",
-            div { class: "text-center",
-                h1 { class: "text-6xl font-bold text-white mb-8", "Hellopod 🐳" }
-                div { class: "text-gray-300 text-lg space-y-2 max-w-md text-left mb-16",
-                    for (meta , meta_data) in metas {
-                        p { class: "",
-                            span { class: "font-semibold inline-block w-64", "{meta}: " }
-                            "{meta_data}"
-                        }
+            h1 { class: "text-center text-6xl font-bold text-white mb-8", "Hellopod 🐳" }
+            div { class: "text-gray-300 text-lg space-y-2 max-w-2xl text-left mb-16",
+                for (meta , meta_data) in metas {
+                    p { key: "{meta}", class: "",
+                        span { class: "font-semibold inline-block w-3xs", "{meta}: " }
+                        "{meta_data}"
                     }
                 }
-                p { class: "text-white text-md",
-                    "Built with ❤️‍🩹 by "
-                    a {
-                        class: "font-medium text-blue-600 dark:text-blue-500 hover:underline",
-                        href: "https://github.com/mirzahilmi",
-                        target: "_blank",
-                        "github.com/mirzahilmi"
-                    }
-                    " w/ Dioxus"
+            }
+            p { class: "text-white text-md",
+                "Built with ❤️‍🩹 by "
+                a {
+                    class: "font-medium text-blue-600 dark:text-blue-500 hover:underline",
+                    href: "https://github.com/mirzahilmi",
+                    target: "_blank",
+                    "github.com/mirzahilmi"
                 }
+                " w/ Dioxus"
             }
         }
     }
