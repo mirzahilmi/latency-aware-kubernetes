942df5635b9abadf767e7514f2a6697bc4682944
mirza@t4nix <dev@mrz.my.id>
2025-08-17 12:04:25 +0700

    feat(prober): discover latency and set backend weight on cilium agent

--- prober/src/main.rs
+++ prober/src/main.rs
@@ -1,80 +1,36 @@
-use std::{env, time::Duration};
-use tracing::{debug, info};
-
-use ::prober::Prober;
-use k8s_openapi::api::core::v1::Node;
-use kube::{Api, Client, Resource, api::ListParams, core::Expression};
-use prober::{Ping, prober::LatencyAggr};
-use prost::Message;
-use rumqttc::{AsyncClient, MqttOptions, QoS};
+use anyhow::Result;
+use prober::{cilium, probe};
+use std::{collections::HashMap, env, time::Duration};
+use tokio::time;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
     let delay = env::var("INTERVAL_IN_SECONDS")?.parse()?;
-    loop {
-        probe().await?;
-        tokio::time::sleep(Duration::from_secs(delay)).await;
-    }
-}
+    let mut cost_caches = HashMap::new();
 
-async fn probe() -> Result<(), Box<dyn std::error::Error>> {
-    info!("Program starting...");
-    let client = Client::try_default().await?;
-    let nodes = Api::<Node>::all(client.clone());
-
-    let exp = Expression::DoesNotExist("node-role.kubernetes.io/control-plane".to_string()).into();
-    let matcher = ListParams::default().labels_from(&exp);
-    let nodes = nodes.list_metadata(&matcher).await?;
-
-    let mut targets = Vec::with_capacity(nodes.iter().count());
-    for node in nodes.iter() {
-        let Some(name) = node.meta().name.clone() else {
-            continue;
-        };
-        targets.push(name);
-    }
-    debug!("Receive probing targets {:?}", targets);
-
-    let k8s_node = env::var("KUBERNETES_NODE")?;
-    let mqtt_host = env::var("MQTT_HOST")?;
-    let mqtt_port = env::var("MQTT_PORT")?.parse()?;
-    let mqtt_result_topic = env::var("MQTT_RESULT_TOPIC")?;
-    let mqtt_client_id = format!("mirzaganteng-{k8s_node}");
-
-    let mut mqtt_options = MqttOptions::new(mqtt_client_id, mqtt_host, mqtt_port);
-    mqtt_options.set_keep_alive(Duration::from_secs(5));
-
-    let (client, mut conn) = AsyncClient::new(mqtt_options, 10);
-    client
-        .subscribe(&mqtt_result_topic, QoS::AtLeastOnce)
-        .await?;
-    info!("Connected to MQTT broker");
-
-    tokio::spawn(async move {
-        info!("MQTT connection pooling started");
-        while conn.poll().await.is_ok() {}
-    });
-
-    let prober = Ping { targets };
-
-    info!("Probe starting...");
-    let latencies = prober.ping(5)?;
-    let aggr = LatencyAggr {
-        node_source: k8s_node,
-        latencies,
+    let retry_threshold: u32 = match env::var("RETRY_THRESHOLD") {
+        Ok(val) => val.parse().unwrap_or(3),
+        Err(_) => 3,
     };
-    debug!("Received probe result {:?}", &aggr.latencies);
 
-    client
-        .publish(
-            &mqtt_result_topic,
-            QoS::ExactlyOnce,
-            false,
-            aggr.encode_to_vec(),
-        )
-        .await?;
-    info!("Probe result published into MQTT broker");
+    let ping_count: u32 = match env::var("PING_COUNT") {
+        Ok(val) => val.parse().unwrap_or(5),
+        Err(_) => 5,
+    };
 
-    Ok(())
+    let node_name = env::var("NODE_NAME")?;
+    let service_name = env::var("CILIUM_SERVICE_NAME")?;
+
+    loop {
+        probe::probe(retry_threshold, ping_count, &mut cost_caches).await;
+
+        if let Err(e) =
+            cilium::update_cilium_service_weights(&node_name, &service_name, &cost_caches).await
+        {
+            tracing::error!("Failed to update cilium service weights: {}", e);
+        }
+
+        time::sleep(Duration::from_secs(delay)).await;
+    }
 }
