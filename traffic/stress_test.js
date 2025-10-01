993b383548dced6e0a1c3131190dece7601f8cad
mirza@t4nix <dev@mrz.my.id>
2025-10-01 14:05:49 +0700

    feat(traffic): stress test

--- traffic/stress_test.js
+++ traffic/stress_test.js
@@ -3,12 +3,18 @@ import { sleep, check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: "1m", target: 50 },
-    { duration: "30s", target: 50 },
-    { duration: "50s", target: 150 },
-    { duration: "3m", target: 200 },
-    { duration: "2m", target: 300 },
-    { duration: "24h", target: 300 },
+    { duration: '10m', target: 100 },
+    { duration: '50m', target: 100 },
+    { duration: '10m', target: 500 },
+    { duration: '1h50m', target: 500 },
+    { duration: '2h15m', target: 1500 },
+    { duration: '1h45m', target: 1500 },
+    { duration: '2h20m', target: 3000 },
+    { duration: '3h40m', target: 3000 },
+    { duration: '1h30m', target: 6000 },
+    { duration: '4h30m', target: 6000 },
+    { duration: '2h30m', target: 10000 },
+    { duration: '2h30m', target: 10000 },
   ],
 };
 
