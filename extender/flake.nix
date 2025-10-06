edfb07fdb7ec0a66c74f7e3d83eabe7daa9fb525
mirza@t4nix <dev@mrz.my.id>
2025-10-06 14:04:32 +0700

    chore(flake): flake and gitignore

---
+++ extender/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [inputs.self.overlays.default];
+          };
+        });
+  in {
+    overlays.default = import inputs.rust-overlay;
+
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          pkg-config
+          (rust-bin.stable."1.86.0".default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          })
+          cargo-machete
+          openssl
+          lldb
+        ];
+      };
+    });
+  };
+}
