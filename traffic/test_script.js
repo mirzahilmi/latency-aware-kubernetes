0d47cd4cd2f2dbaaac6e592a3d252ca9854684fe
mirza@t4nix <dev@mrz.my.id>
2025-11-16 18:56:05 +0700

    test: custom test to eval system

---
+++ traffic/test_script.js
@@ -0,0 +1,28 @@
+import http from "k6/http";
+import { SharedArray } from 'k6/data';
+import { sleep, check } from "k6";
+
+const nodes = new SharedArray("nodes", function () {
+  const nodes = JSON.parse(open("./targets.json"));
+  return nodes;
+});
+
+if (nodes.length == 0)
+  throw "NODES IS EMPTY";
+
+var scenarios = {};
+scenarios[`${nodes[0].hostname}`] = {
+  executor: "constant-vus",
+  duration: "5m",
+  vus: __ENV.VUS,
+  env: { TARGET: nodes[0].ip },
+};
+export const options = {
+  scenarios: scenarios,
+  discardResponseBodies: true,
+};
+
+export default function() {
+  const res = http.get(`http://${__ENV.TARGET}:30000`);
+  check(res, { "status is 200": (res) => res.status === 200 });
+}
